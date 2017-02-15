5bc14bbc6278ba9ba9a2ed91611c4dee5d4d1374
Will Teeple <william.j.teeple@ku.edu>
2017-02-15 15:17:19 -0600

    Add file descriptor check and minor fixes.

--- GPS_Decoder/main.cpp
+++ GPS_Decoder/main.cpp
@@ -16,10 +16,8 @@ int main()
 {
 	float latInMin=1, latInMax=1, lonInMin=1, lonInMax=1, latOutMin=1, latOutMax=1, lonOutMin=1, lonOutMax =1;
 
-
-
 	//program initialization
-/**
+	/**
 	std::cout << "Welcome to GPS test program!\n";
 	std::cout << "Enter inner window latitude MIN: "; std::cin >> latInMin;
 	std::cout << "Enter inner window latitude MAX: "; std::cin >> latInMax;
@@ -30,39 +28,46 @@ int main()
 	std::cout << "Enter outer window longitude MIN: "; std::cin >> lonOutMin;
 	std::cout << "Enter outer window longitude MAX: "; std::cin >> lonOutMax;
 	**/
-	//initialize GPS handler
-	GPSModule gps(latInMin, latInMax, lonInMin, lonInMax, latOutMin, latOutMax, lonOutMin, lonOutMax);
 
 	//open serial input for GPS device
 	// /dev/ttyS0
-	// /dev/sdb or /dev/sdb1
 	int gpsSerial = open("/dev/ttyUSB0", O_RDONLY);
-	char c = ' ';	
+
+	if (gpsSerial == -1)
+	{
+		std::cerr << "Failed to open port to GPS. Exiting...\n\n";
+		exit(EXIT_FAILURE);
+	}
+
+	//initialize GPS handler
+	GPSModule gps(latInMin, latInMax, lonInMin, lonInMax, latOutMin, latOutMax, lonOutMin, lonOutMax);
+
+	char c = ' ';
 	std::string gpsString = "";
-	
-	//TODO: Error checking for opening serial port
+
 	do
 	{
 		//read in each character
 		read(gpsSerial, &c, 1);
-
+		std::cout << c << std::endl;
 		//check for new GPS string
 		if (c == '$')
 		{
 			if (gpsString.find('$') != std::string::npos)
 			{
 				if(gps.gpsStringCounter % 5 == 0)
-					gps.findCoordPositionsInString(gpsString);
-				gps.updatePosition(gpsString);
+					//gps.findCoordPositionsInString(gpsString);
+				//gps.updatePosition(gpsString);
+				std::cout << gpsString << std::endl;
 			}
 			gpsString = "$";
 		}
-		else if (isalnum(c) || c ==',')
+		else if (isprint(c))
 		{
 			gpsString += c;
 		}
-		
-	} while(true); 
+
+	} while(true);
 	//TODO: Close USB port???
 
 	return 0;
